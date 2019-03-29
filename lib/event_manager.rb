0519ff59906777441e0ba89479e501671350ff02
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-03-29 16:05:30 +0300

    Clean Zipcodes
    - Upon data visualization we realized there are sum missing zipcodes, some with less than 5 digits & others with more than 5.
    - Thus we created a `.clean_zipcodes` function that makes all zipcodes five(5) digits.
    
    JIRA: BEDT-231

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,11 @@
 require "csv"
+
+def clean_zipcode(zipcode)
+  # if the zip code is exactly five(5) digits, assume that it is ok
+  # if the zip code is more than five(5) digits, truncate it to the first 5 digits
+  # if the zip code is less than five(5) digits, add zeros(0) to the front until it becomes five digits
+  zipcode.to_s.rjust(5, "0")[0..4]
+end
 puts "--------------------------------------------------"
 puts "Event Manager Initialized".center(50)
 puts "--------------------------------------------------"
@@ -23,6 +30,7 @@ puts
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
   first_name = row[:first_name]
-  zip_code = row[:zipcode]
+  zip_code = clean_zipcode(row[:zipcode])
+
   puts "#{ first_name } : #{ zip_code }"
 end
