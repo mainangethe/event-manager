d2b7647e8a10ff1c9d3447220fc9a7e83bb34c2b
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-03-29 15:35:40 +0300

    Convert file headers into :symbols
    - Ruby's CSV parser allows us to access the column values by their headers.
    - Our CSV file defines several different formats for the column names.
    - Converting the headers to symbols will make our column names more uniform and easier to remember.
    - Example: the header `first_Name` will be converted to `:first_name`.
    
    JIRA: BEDT-231

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -20,8 +20,8 @@ puts
 # Use a CSV parser
 # It will have more options beyond our custom one
 
-contents = CSV.open "event_attendees.csv", headers: true
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
-  name = row[2]
+  name = row[:first_name]
   puts name
 end
