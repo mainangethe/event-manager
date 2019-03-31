62eb91eadb7c8e5bd6212538305d1a15a3c2f1d1
Ng'ethe Maina <ngethe@mradi.co.ke>
2019-03-31 17:08:45 +0300

    Add time-tracking method
    We created a function that takes and array;loops over it and counts the number of times a unique element appeared.
    
    JIRA: BEDT-231

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -28,6 +28,12 @@ def clean_phonenumber phonenumber
   end
   phonenumber
 end
+
+def time_tracking registration_hour_array
+  registration_hour_array.uniq.each do |element|
+    puts "Registered at #{element}: #{ registration_hour_array.count(element) } time(s)"
+  end
+end
 puts "--------------------------------------------------"
 puts "Event Manager Initialized".center(50)
 puts "--------------------------------------------------"
@@ -51,6 +57,7 @@ puts
 
 template_letter = File.read "form_letter.erb"
 erb_template = ERB.new template_letter
+hours_array = []
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
@@ -60,10 +67,11 @@ contents.each do |row|
   phonenumber = clean_phonenumber row[:homephone]
   american_date = "%m/%d/%y %H:%M"
   registration_date = DateTime.strptime(row[:regdate], american_date)
+  hours_array << registration_date.hour
 
-
-  puts "#{ first_name }: Registered at #{ registration_date.hour }"
-
+  # puts "#{ first_name }: Registered at #{ registration_date.hour }"
   form_letter = erb_template.result(binding)
   # save_thank_you_letters(id, form_letter)
 end
+
+time_tracking hours_array
